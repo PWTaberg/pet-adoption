13f5feb4ecf22c0c015de7d4ab1321211f103724
Peter <pwelliver@yahoo.com>
2024-03-25 09:21:11 +0100

    turned pet card into a JS Template

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// Template
+const template = document.querySelector("#pet-card-template");
+const wrapper = document.createDocumentFragment();
+
 // Dynamic Weather Info
 
 const weatherUrl = "https://api.weather.gov/gridpoints/MFL/110,50/forecast";
@@ -33,8 +37,13 @@ async function petsArea() {
   console.log(petsData[0]);
 
   petsData.forEach(pet => {
-    console.log(pet.name);
+    const clone = template.content.cloneNode(true);
+    clone.querySelector("h3").textContent = pet.name;
+
+    wrapper.appendChild(clone);
   });
+
+  document.querySelector(".list-of-pets").appendChild(wrapper);
 }
 
 petsArea();
