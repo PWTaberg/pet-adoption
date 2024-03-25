4ec79c7be0a022a048df24e789041e37f70206f3
Peter <pwelliver@yahoo.com>
2024-03-25 14:40:37 +0100

    pet area finished

--- main.js
+++ main.js
@@ -38,6 +38,9 @@ async function petsArea() {
 
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true);
+
+    // tag each pet-card with its species
+    clone.querySelector(".pet-card").dataset.species = pet.species;
     clone.querySelector("h3").textContent = pet.name;
     clone.querySelector(".pet-description").textContent = pet.description;
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear);
@@ -81,4 +84,13 @@ function handleButtonClick(e) {
   // add active class to jest clicked button
   e.target.classList.add("active");
   // actually filter pets based on selection
+  const currentFilter = e.target.dataset.filter;
+
+  document.querySelectorAll(".pet-card").forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid";
+    } else {
+      el.style.display = "none";
+    }
+  });
 }
